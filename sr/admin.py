38e6802246f5dd93e2174c8f526572d109c67b38
mikhail kondratyev <fatune@gmail.com>
2017-09-30 00:22:13 +1100

    register fact and card on admin page

--- sr/admin.py
+++ sr/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Subject
+from .models import Subject, Fact, Card
 
 admin.site.register(Subject)
+admin.site.register(Fact)
+admin.site.register(Card)

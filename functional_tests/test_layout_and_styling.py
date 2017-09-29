3729c36f5460115c1ce06f6795f981ca6f0d09c2
mikhail kondratyev <fatune@gmail.com>
2017-09-29 19:27:36 +1100

    FT checks if user sees link to study on a home page

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+from .base import FunctionalTest
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        # User goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        ## User notices the input box is nicely centered
+        #inputbox = self.browser.find_element_by_id('id_new_item')
+        #self.assertAlmostEqual(
+        #    inputbox.location['x'] + inputbox.size['width'] / 2,
+        #    512,
+        #    delta=150
+        #)
+
---
+++ functional_tests/test_study.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from .base import FunctionalTest
+
+
+class NewVisitorTest(FunctionalTest):
+
+    def test_user_begin_studying_for_the_1st_tim(self):
+        # User visits homepage
+        self.browser.get(self.live_server_url)
+
+        # User notices the page title and header mention Geology 
+        self.assertIn('Geology', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Geology', header_text)
+
+        # User is invited to follow a link to study a NE subject
+        link = self.browser.find_element_by_id('id_link_to_study')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Study NE subject'
+        )
+
+        # User follows the link and notices that link has changed
+        link.click()
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/study/.+')
+
+        # User reads a fancy introduction 
+
+        # User hits enter and sees a first question map
+
+        # User clicks button and sees an answer to the first question map
+        # and two buttons "Bad" and "Good"
+
+        # User clicks "Bad" button and sees a first question map again
+
+        # User clicks button and sees an answer to the first question map
+        # and two buttons "Bad" and "Good"
+
+        # Now User clicks "Good" button and sees second question map 
+
+        # User clicks button and sees an answer to the first question map
+        # and two buttons "Bad" and "Good"
+
+        # Now User clicks "Good" button and sees third question with texts with blank words
+
+        # User clicks button and sees and anwer to the third question
+
+        # User clicks "Good" button and sees final screen with greetings
